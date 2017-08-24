ce0d7da87b983ef2eb8cfdd41d1182a8e2c39154
Zach VanSomeren <23041336+zvansom@users.noreply.github.com>
2017-08-24 12:32:06 -0600

    HTML button added.  JS work in progress
    JS functions not fully operational.  Temp and Unit variables not updating outside of functions.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-  var api = "https://fcc-weather-api.glitch.me/api/current?"
+  var api = "https://fcc-weather-api.glitch.me/api/current?";
   var lat, lon;
   var temp = {};
-  var unit = "C"
+  var unit = "C";
 
   // Retrieve location from user and send to API
   if (navigator.geolocation) {
@@ -25,10 +25,40 @@ $(document).ready(function() {
         $("#temp").html(temp + " " + unit);
         $("#description").html(result.weather[0].main);
         $("#icon").html("<img src='" + result.weather[0].icon + "'>");
+        return temp;
       }
     });
   }
 
-      console.log(temp);
+  $("button").on("click", function() {
+    alert(temp+" "+ unit);
+    convertTemp(temp, unit);
+  });
+
+  function convertTemp(temp, unit) {
+    if (unit === "F"){
+      fahrenheitToCelcius(temp, unit);
+    } else if (unit === "C") {
+      celciusToFahrenheit(temp, unit);
+    }
+  }
+
+  function celciusToFahrenheit(temp, unit) {
+    temp = (temp * 1.8) + 32;
+    unit = "F";
+    $("#temp").html(temp + " " + unit);
+    alert(unit);
+    return temp, unit;
+  }
+
+  function fahrenheitToCelcius(temp, unit) {
+    alert("This function works");
+    temp = (temp - 32)/1.8;
+    unit = "C"; 
+    $("#temp").html(temp + " " + unit);
+    return temp, unit;
+  }
+
+
 
 });
