7da3744bbd7110ae8c4c8a5d31d469066fc733f9
Zach VanSomeren <23041336+zvansom@users.noreply.github.com>
2017-08-25 11:20:13 -0600

    Formulas corrected in main.js. CSS added

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   var api = "https://fcc-weather-api.glitch.me/api/current?";
   var lat, lon;
-  var temp = {};
   var unit = "C";
 
   // Retrieve location from user and send to API
@@ -21,44 +20,25 @@ $(document).ready(function() {
       url: urlString,
       success: function(result){
         $("#location").html(result.name);
-        temp = result.main.temp
+        temp = result.main.temp;
         $("#temp").html(temp + " " + unit);
         $("#description").html(result.weather[0].main);
         $("#icon").html("<img src='" + result.weather[0].icon + "'>");
-        return temp;
       }
     });
   }
 
   $("button").on("click", function() {
-    alert(temp+" "+ unit);
-    convertTemp(temp, unit);
-  });
-
-  function convertTemp(temp, unit) {
     if (unit === "F"){
-      fahrenheitToCelcius(temp, unit);
-    } else if (unit === "C") {
-      celciusToFahrenheit(temp, unit);
+      temp = (temp - 32)/1.8;
+      unit = "C";
+      $("#temp").html(temp.toFixed(2) + " " + unit);
     }
-  }
-
-  function celciusToFahrenheit(temp, unit) {
-    temp = (temp * 1.8) + 32;
-    unit = "F";
-    $("#temp").html(temp + " " + unit);
-    alert(unit);
-    return temp, unit;
-  }
-
-  function fahrenheitToCelcius(temp, unit) {
-    alert("This function works");
-    temp = (temp - 32)/1.8;
-    unit = "C"; 
-    $("#temp").html(temp + " " + unit);
-    return temp, unit;
-  }
-
-
+    else if (unit === "C") {
+        temp = (temp * 1.8) + 32;
+        unit = "F";
+        $("#temp").html(temp.toFixed(2) + " " + unit);
+    }
+  });
 
 });
